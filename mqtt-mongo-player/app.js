45785639a820b2df5d0b5701498ba9f53926abb4
Özgür Keleş <ozgur.keles@ries.com.tr>
2016-12-28 14:46:31 +0300

    temel yapı kopyalandı

--- mqtt-mongo-player/app.js
+++ mqtt-mongo-player/app.js
@@ -1,1 +1,42 @@
-﻿console.log('Hello world');
+﻿var mongodb = require('mongodb');
+var mqtt = require('mqtt');
+var mqtt_regex = require("mqtt-regex");
+var config = require('./config');
+
+var mqttUri = 'mqtt://' + config.mqtt.hostname + ':' + config.mqtt.port;
+var client = mqtt.connect(mqttUri);
+
+client.on('connect', function () {
+    client.subscribe(config.mqtt.namespace);
+});
+
+var mongoUri = 'mongodb://' + config.mongodb.hostname + ':' + config.mongodb.port + '/' + config.mongodb.database;
+mongodb.MongoClient.connect(mongoUri, function (error, database) {
+    if (error != null) {
+        throw error;
+    }
+
+    var collection = database.collection(config.mongodb.collection);
+    collection.createIndex({ "topic": 1, "ts": -1 });
+
+    client.on('message', function (topic, message) {
+        var json = {};
+        try {
+            json = JSON.parse(String(message));
+        } catch (e) {
+            console.log(e);
+        }
+        var messageObject = {
+            ts: new Date(),
+            topic: topic,
+            message: message.toString(),
+            json: json
+        };
+
+        collection.insert(messageObject, function (error, result) {
+            if (error != null) {
+                console.log("ERROR: " + error);
+            }
+        });
+    });
+});
